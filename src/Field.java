02603211f569253355b931ed3e08f4d45dadf120
Tymoteusz Frankiewicz <tymoteusz@frankiewicz.com.pl>
2016-08-03 15:00:16 +0200

    Added computer player

--- src/Field.java
+++ src/Field.java
@@ -4,8 +4,8 @@ import java.awt.GridLayout;
 
 public class Field extends JFrame{
 	
-	MyButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
-	MyButton[] buttonTab = {button1, button2, button3, button4, button5, button6, button7, button8, button9};
+	public static MyButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
+	public static MyButton[] buttonTab = {button1, button2, button3, button4, button5, button6, button7, button8, button9};
 
 	public static void main(String[] args){
 		new Field();
@@ -16,6 +16,7 @@ public class Field extends JFrame{
 		this.setTitle("Game");
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
 		GridLayout gridLayout = new GridLayout(3,3);
 		setLayout(gridLayout);
 		for(int i = 0; i < 9; ++i){
@@ -23,5 +24,36 @@ public class Field extends JFrame{
 			add(buttonTab[i]);
 		}		
 		this.setVisible(true);
+		win();
+	}
+	
+	public void win(){
+		if(((Field.button1.getIcon() == MyButton.xImg) && (Field.button2.getIcon() == MyButton.xImg) && (Field.button3.getIcon() == MyButton.xImg)) 
+		|| ((Field.button4.getIcon() == MyButton.xImg) && (Field.button5.getIcon() == MyButton.xImg) && (Field.button6.getIcon() == MyButton.xImg))
+		|| ((Field.button7.getIcon() == MyButton.xImg) && (Field.button8.getIcon() == MyButton.xImg) && (Field.button9.getIcon() == MyButton.xImg))
+		|| ((Field.button1.getIcon() == MyButton.xImg) && (Field.button5.getIcon() == MyButton.xImg) && (Field.button9.getIcon() == MyButton.xImg))
+		|| ((Field.button3.getIcon() == MyButton.xImg) && (Field.button5.getIcon() == MyButton.xImg) && (Field.button7.getIcon() == MyButton.xImg))
+		|| ((Field.button1.getIcon() == MyButton.xImg) && (Field.button4.getIcon() == MyButton.xImg) && (Field.button7.getIcon() == MyButton.xImg))
+		|| ((Field.button2.getIcon() == MyButton.xImg) && (Field.button5.getIcon() == MyButton.xImg) && (Field.button8.getIcon() == MyButton.xImg))
+		|| ((Field.button3.getIcon() == MyButton.xImg) && (Field.button6.getIcon() == MyButton.xImg) && (Field.button9.getIcon() == MyButton.xImg))){
+			JOptionPane.showMessageDialog(Field.this, "You won", "You won", JOptionPane.INFORMATION_MESSAGE);
+			// player won				  
+		}
+		else if(((Field.button1.getIcon() == MyButton.oImg) && (Field.button2.getIcon() == MyButton.oImg) && (Field.button3.getIcon() == MyButton.oImg)) 
+		|| ((Field.button4.getIcon() == MyButton.oImg) && (Field.button5.getIcon() == MyButton.oImg) && (Field.button6.getIcon() == MyButton.oImg))
+		|| ((Field.button7.getIcon() == MyButton.oImg) && (Field.button8.getIcon() == MyButton.oImg) && (Field.button9.getIcon() == MyButton.oImg))
+		|| ((Field.button1.getIcon() == MyButton.oImg) && (Field.button5.getIcon() == MyButton.oImg) && (Field.button9.getIcon() == MyButton.oImg))
+		|| ((Field.button3.getIcon() == MyButton.oImg) && (Field.button5.getIcon() == MyButton.oImg) && (Field.button7.getIcon() == MyButton.oImg))
+		|| ((Field.button3.getIcon() == MyButton.oImg) && (Field.button5.getIcon() == MyButton.oImg) && (Field.button7.getIcon() == MyButton.oImg))
+		|| ((Field.button1.getIcon() == MyButton.oImg) && (Field.button4.getIcon() == MyButton.oImg) && (Field.button7.getIcon() == MyButton.oImg))
+		|| ((Field.button2.getIcon() == MyButton.oImg) && (Field.button5.getIcon() == MyButton.oImg) && (Field.button8.getIcon() == MyButton.oImg))
+		|| ((Field.button3.getIcon() == MyButton.oImg) && (Field.button6.getIcon() == MyButton.oImg) && (Field.button9.getIcon() == MyButton.oImg))){
+			JOptionPane.showMessageDialog(Field.this, "Computer won", "Computer won", JOptionPane.INFORMATION_MESSAGE);
+			// computer won
+		}
+		
+		else JOptionPane.showMessageDialog(Field.this, "Draw", "Draw", JOptionPane.INFORMATION_MESSAGE);
+		
+		
 	}
 }
--- src/MyButton.java
+++ src/MyButton.java
@@ -1,17 +1,34 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 
 public class MyButton extends JButton {
 	int imgCounter = 0;
+	int[] tabOfIndexes = new int[9];
 	ImageIcon whiteImg = new ImageIcon("resources/white.jpg");
-	ImageIcon xImg = new ImageIcon("resources/x.png");
-	ImageIcon oImg = new ImageIcon("resources/o.png");
+	public static ImageIcon xImg = new ImageIcon("resources/x.png");
+	public static ImageIcon oImg = new ImageIcon("resources/o.png");
 	
 	
 	public MyButton(ImageIcon img){
 		this.addActionListener(new ListenForButton());
 		this.setIcon(img);
+	}
+	
+	public void markFieldwithO(){
+		int i = 0;
+		while(!Field.buttonTab[i].isEmpty()){
+			++i;
+		}
+		Field.buttonTab[i].setIcon(oImg);
+	}
+	
+	public boolean isEmpty(){
+		if(this.getIcon() == whiteImg) return true; //TODO why it doesn't work with imgCounter 
+		else return false;
 	}
 	
 	public MyButton(){
@@ -32,8 +49,9 @@ private class ListenForButton implements ActionListener{
 			case 2: setIcon(oImg);
 			break;
 			}
+			markFieldwithO();
+			
 		}
 	
 	}
-
 }
